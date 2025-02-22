f47f7638dfeabd48f271307f0f2fc368fd8ce244
TanishkaCh30 <137798637+TanishkaCh30@users.noreply.github.com>
2025-02-22 23:48:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,23 +5,40 @@ let username = document.getElementById("username");
 let password = document.getElementById("password");
 
 
-submitBtn.addEventListener("click" ,(e)=>{
-	e.preventDefault();
-	if(username.value!=="" && password.value!=="" && !checkBox.checked){
-		 localStorage.removeItem('credentials');
-		return;
-	}
-	else if(username.value!=="" && password.value!=="" && checkBox.checked){
-		let credentials = {
-	username:username.value,
-	password:password.value
-}
+// submitBtn.addEventListener("click" ,(e)=>{
+// 	e.preventDefault();
+// 	if(username.value!=="" && password.value!=="" && !checkBox.checked){
+// 		 localStorage.removeItem('credentials');
+// 		return;
+// 	}
+// 	else if(username.value!=="" && password.value!=="" && checkBox.checked){
+// 		let credentials = {
+// 	username:username.value,
+// 	password:password.value
+// }
 
-		localStorage.setItem('credentials', JSON.stringify(credentials));
-		// location.reload();
-		existBtn.style.display = 'block';
-	}
-})
+// 		localStorage.setItem('credentials', JSON.stringify(credentials));
+// 		// location.reload();
+// 		existBtn.style.display = 'block';
+// 	}
+// })
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (username.value !== "" && password.value !== "" && !checkBox.checked) {
+    localStorage.clear();
+  } 
+  else if (username.value !== "" && password.value !== "" && checkBox.checked) {
+    let formData = {
+      'username':username.value,
+      'password':password.value
+    }
+    localStorage.setItem('formData',JSON.stringify(formData));
+    existBtn.style.display = 'block';
+  }
+  alert(`Logged in as ${username.value}`);
+});
+
 
 existBtn.addEventListener("click" ,()=>{
 	let data = localStorage.getItem('credentials');
