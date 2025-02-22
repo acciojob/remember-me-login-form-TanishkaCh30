66a595c77f101d99df7b2d20b0996ff368660944
TanishkaCh30 <137798637+TanishkaCh30@users.noreply.github.com>
2025-02-22 22:59:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-//your JS code here. If required.
+let form = document.getElementById("form");
+let submitBtn =  document.getElementById("submit");
+let existBtn =  document.getElementById("existing");
+let checkBox = document.getElementById("checkbox");
+let username = document.getElementById("username");
+let password = document.getElementById("password");
+
+
+
+submitBtn.addEventListener("click" ,(e)=>{
+	e.preventDefault();
+	if(username.value!=="" && password.value!=="" && !checkBox.checked){
+		localStorage.clear();
+		alert("credentials are not stored");
+	}
+	else if(username.value!=="" && password.value!=="" && checkBox.checked){
+		let formData = {
+	'username':username.value,
+	'password':password.value
+}
+
+		localStorage.setItem('formData',JSON.stringify(formData));
+		// location.reload();
+		existBtn.style.display = 'block';
+	}
+})
+
+existBtn.addEventListener("click" ,()=>{
+	let data = localStorage.getItem('formData');
+	if(data){
+	let parsedData = JSON.parse(data);
+	alert(`Logged in as ${parsedData.username}`);
+	}
+})
+
